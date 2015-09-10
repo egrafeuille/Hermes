c7df9554fa3b9e04cf2041806d095f117659d9b1
egrafeuille <ernesto.grafeuille@gmail.com>
2015-09-10 15:10:52 -0300

    User management updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_mode
+
+  # GET /users
+  def index
+    @users = User.players.where(trainer_id: current_user.id)
+    # @groups = Group.all
+  end
+
+  # GET /users/1
+  def show
+  end
+    
+  # GET /users/new
   def new
     @user = User.new
   end
   
+  # GET /users/1/edit
+  def edit
+  end
+  
+  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,11 +31,38 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  # PATCH/PUT /users/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
   
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    end
+  end
+
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :birthDate, :sex, :type, :trainer_id)
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
     end
   
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :birthDate, :sex, :type, :trainer_id, :group_id)
+    end
+    
+    def set_mode
+      @trainer_mode = current_user.type == 'Trainer'
+    end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :groups          #, -> { where(type: 'Trainer') }
+  belongs_to :group         #, -> { where(type: 'Player') }
+  belongs_to :trainer, class_name: "User", foreign_key: "trainer_id"
   
   validates :username, presence: true, uniqueness: true  
-  validates :password, presence: true, confirmation: true, length: { in: 6..20 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, confirmation: true, length: { in: 6..20 }, :on => :create
+  validates :password_confirmation, presence: true, :on => :create
   validates :email, presence: true
   validates :sex, inclusion: { in: %w(M F U) }
   validates :type, inclusion: { in: %w(Trainer Player) }
